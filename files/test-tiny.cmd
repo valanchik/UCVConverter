770b84c35a2b6ff5e076844e50881cd826ca9db4
admin <admin@vovanchik.ru>
2022-04-07 16:04:38 +0300

    add files

---
+++ files/test-tiny.cmd
@@ -0,0 +1,7 @@
+echo     Example how to start training Darknet Yolo v2 for 2 objects (air, bird)
+
+
+darknet.exe detector map data\obj.data yolov4-tiny.cfg backup\yolov4-tiny_last.weights
+
+
+pause
---
+++ files/train-tiny_cudnn.cmd
@@ -0,0 +1,7 @@
+echo     Example how to start training Darknet Yolo v2 for 2 objects (air, bird)
+
+
+darknet.exe detector train data/obj.data yolov4-tiny.cfg yolov4-tiny.conv.29 -map
+
+
+pause
