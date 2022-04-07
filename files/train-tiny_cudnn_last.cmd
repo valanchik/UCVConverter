42e6f161a6ce463aeb60944cb4ea1baa23a1d1d4
admin <admin@vovanchik.ru>
2022-04-07 16:07:08 +0300

    Revert "add files"
    This reverts commit 770b84c35a2b6ff5e076844e50881cd826ca9db4.

--- files/train-tiny_cudnn_last.cmd
+++
@@ -1,7 +0,0 @@
-echo     Example how to start training Darknet Yolo v2 for 2 objects (air, bird)
-
-
-darknet.exe detector train data/obj.data yolov4-tiny.cfg backup\yolov4-tiny_last.weights -map
-
-
-pause
